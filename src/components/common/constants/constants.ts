25fd2ae1ff91cdb09fe666a2da562b426b65bb34
marcusfreirepaulino <marcusfreirepaulino@gmail.com>
2023-03-07 22:26:36 -0300

    create constants file and add style-guid

---
+++ src/components/common/constants/constants.ts
@@ -0,0 +1,39 @@
+export class Color {
+  public static readonly Black = '#1F1923';
+  public static readonly White = '#FFFFF0';
+  public static readonly Grey = '#9A9A95';
+  public static readonly Coal = '#36454F';
+  public static readonly Red = '#B60000';
+  public static readonly Brown = '#8B5A2B';
+  public static readonly DarkBrown = '#9B2C1B';
+  public static readonly Gold = '#D4AF37';
+  public static readonly Green = '#1E760F';
+
+  public static readonly Background = '#FEEFDA';
+  public static readonly TrasnparentBlack = 'rgba(31, 25, 35, 0.5)';
+  public static readonly InputText = '#353535';
+}
+
+export enum FontFamily {
+  Primary = 'Roboto',
+}
+
+export enum FontWeight {
+  Regular = 400,
+}
+
+export enum FontSizeDesktop {
+  XLarge = '36px',
+  Large = '24px',
+  Medium = '16px',
+  Small = '14px',
+  XSmall = '10px',
+}
+
+export enum FontSizeMobile {
+  XLarge = '24px',
+  Large = '16px',
+  Medium = '12px',
+  Small = '10px',
+  XSmall = '6px',
+}
