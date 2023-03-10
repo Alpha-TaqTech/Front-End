9e0dbf0aef3cb9ab7a299276e3bd4c8cacb6d8e0
marcusfreirepaulino <marcusfreirepaulino@gmail.com>
2023-03-10 15:20:55 -0300

    text area component

--- src/components/common/inputs/inputs-components.styled.ts
+++ src/components/common/inputs/inputs-components.styled.ts
@@ -7,14 +7,34 @@ import {
   FontSizeMobile,
 } from '../constants';
 
-export const TextField = styled.input`
+interface TextFieldProps {
+  width?: number;
+}
+
+interface TextAreaProps extends TextFieldProps {
+  height: number;
+}
+
+export const TextField = styled.input<TextFieldProps>`
   font-family: ${FontFamily.Primary};
   font-size: ${FontSizeDesktop.Medium};
   border: 1px solid ${Color.Black};
   border-radius: ${BorderRadius.Medium};
   background: ${Color.White};
   height: 30px;
-  width: 240px;
+  width: ${({ width }) => width ?? 240}px;
+  outline: none;
+`;
+
+export const TextArea = styled.textarea<TextAreaProps>`
+  font-family: ${FontFamily.Primary};
+  font-size: ${FontSizeDesktop.Medium};
+  border: 1px solid ${Color.Black};
+  border-radius: ${BorderRadius.Medium};
+  background: ${Color.White};
+  width: ${({ width }) => width ?? 240}px;
+  height: ${({height}) => height ?? 90}px;
+  resize: none;
   outline: none;
 `;
 
