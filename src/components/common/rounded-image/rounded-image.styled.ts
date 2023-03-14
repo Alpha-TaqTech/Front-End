eb28c54945ebe52f985ad76a5d8adb84e3383eda
marcusfreirepaulino <marcusfreirepaulino@gmail.com>
2023-03-14 09:32:21 -0300

    fake data

--- src/components/common/rounded-image/rounded-image.styled.ts
+++ src/components/common/rounded-image/rounded-image.styled.ts
@@ -1,12 +1,15 @@
 import styled from 'styled-components';
+import { Color } from '../constants';
 
 interface RoundedImageProps {
   background: string;
   size: number;
+  border?: boolean;
 }
 
 export const RoundedImage = styled.div<RoundedImageProps>`
   border-radius: 50%;
+  border:  ${({border}) => border && '1px solid ' + Color.Black.base };
   background: ${({ background }) => background};
   background-size: contain;
   width: ${({ size }) => size}px;
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,25 +1,52 @@
 import { RoundedImage } from '../../components/common/rounded-image';
 import { Color } from '../../components/common/constants';
 import { Container } from '../../components/Container';
+import { H1, Label } from '../../components/common/typography';
+import { Button } from '../../components/Button';
 
-interface UserProfile {
-  userName: string;
-  userAvatar: string;
-  following: number;
-  followers: number;
-  userPosts: any[];
-}
+const FAKE_DATA = {
+  username: 'username',
+  userImage: 'blue',
+  followers: 41,
+  following: 21,
+  posts: [],
+};
 
 export const Profile = () => {
   return (
     <Container
       backgroundColor={Color.Background.base}
-      height={'250px'}
-      justify={'center'}
+      justify={'start'}
+      align={'start'}
       padding={'10px 16px'}
       gap={'12px'}
     >
-      <RoundedImage
+      <Container
+        direction="row"
+        backgroundColor="transparent"
+        height="fit-content"
+        width="100%"
+        align="start"
+        justify="space-around"
+      >
+        <RoundedImage background={FAKE_DATA.userImage} size={100} border />
+        <Container
+          backgroundColor="transparent"
+          height="fit-content"
+          width="fit-content"
+          align="start"
+        >
+          <Container backgroundColor="transparent" margin="auto">
+            <H1>username</H1>
+          </Container>
+          <Label>Seguidores {FAKE_DATA.followers}</Label>
+          <Label>Seguindo {FAKE_DATA.following}</Label>
+        </Container>
+        <Container backgroundColor="transparent" width="fit-content" gap="16px">
+          <Button color={Color.Green} label="Seguir" />
+          <Button color={Color.Red} label="Bloquear" />
+        </Container>
+      </Container>
     </Container>
   );
 };
